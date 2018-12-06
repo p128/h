386c8ccf1ed579320729612128f74b655ef5b0a6
p128 <30474357+p128@users.noreply.github.com>
2018-12-06 22:55:25 +0530

    Update c1.c

--- c1.c
+++ c1.c
@@ -2,35 +2,34 @@
 #include <stdlib.h>
 #include <math.h>
 
-// CUDA kernel. Each thread takes care of one element of c
+
 __global__ void vecAdd(double *a, double *b, double *c, int n)
 {
-    // Get our global thread ID
+    
     int id = blockIdx.x*blockDim.x+threadIdx.x;
 
-    // Make sure we do not go out of bounds
+ 
     if (id < n)
         c[id] = a[id] + b[id];
 }
 
 int main( int argc, char* argv[] )
 {
-    // Size of vectors
+    
     int n = 10;
 
-    // Host input vectors
+    
     double *h_a;
     double *h_b;
-    //Host output vector
+    
     double *h_c;
 
-    // Device input vectors
+    
     double *d_a;
     double *d_b;
-    //Device output vector
+    
     double *d_c;
 
-    // Size, in bytes, of each vector
     size_t bytes = n*sizeof(double);
 
     // Allocate memory for each vector on host
