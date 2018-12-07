c891757bb80c0a85fc7494244643b2488db249ee
p128 <30474357+p128@users.noreply.github.com>
2018-12-07 08:00:38 +0530

    Update c1.c

--- c1.c
+++ c1.c
@@ -32,52 +32,51 @@ int main( int argc, char* argv[] )
 
     size_t bytes = n*sizeof(double);
 
-    // Allocate memory for each vector on host
+  
     h_a = (double*)malloc(bytes);
     h_b = (double*)malloc(bytes);
     h_c = (double*)malloc(bytes);
-    // Allocate memory for each vector on GPU
+   
     cudaMalloc(&d_a, bytes);
     cudaMalloc(&d_b, bytes);
     cudaMalloc(&d_c, bytes);
 
     int i;
-    // Initialize vectors on host
+  
     for( i = 0; i < n; i++ ) {
         h_a[i] = i;
         h_b[i] = i;
     }
 
-    // Copy host vectors to device
+ 
     cudaMemcpy( d_a, h_a, bytes, cudaMemcpyHostToDevice);
     cudaMemcpy( d_b, h_b, bytes, cudaMemcpyHostToDevice);
 
     int blockSize, gridSize;
 
-    // Number of threads in each thread block
+    
     blockSize = 100;
 
-    // Number of thread blocks in grid
+   
     gridSize = (int)ceil((float)n/blockSize);
 
-    // Execute the kernel
+    
     vecAdd<<<gridSize, blockSize>>>(d_a, d_b, d_c, n);
 
-    // Copy array back to host
+    
     cudaMemcpy( h_c, d_c, bytes, cudaMemcpyDeviceToHost );
 
-    // Sum up vector c and print result divided by n, this should equal 1 within error
+  
     double sum = 0;
     for(i=0; i<n; i++)
         printf(" %f + %f =%f\n",h_a[i],h_b[i],h_c[i]);
-    //printf("final result: %f\n", sum/(double)n);
+ 
 
-    // Release device memory
  cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
 
-    // Release host memory
+  
     free(h_a);
     free(h_b);
     free(h_c);
